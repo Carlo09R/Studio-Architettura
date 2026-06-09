a3e0bab27968f2c2097409228680d43d4218d05f
Carlo Roman <c.roman@donboscosandona.it>
2026-06-09 12:26:59 +0200

    Primo appunto

---
+++ studio-architettura/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Card1 from "@/components/Card1";
+export default function Home() {
+  return (
+   <>
+   <Card1 immagine=" bg-[url(/images/Card1.jpg)]" sottotitolo="" titolo="Studio Orrizzonti" bottone="Scopri Atomo Star" colore=" text-black" coloreb=" text-black"/>
+   </>
+  );
+}
